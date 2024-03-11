8db6cbe285d9b390738dea7b978fdd82b8a0fa7e
Mkmika <mkhoundjenoukon@gmail.com>
2024-03-11 09:36:21 +0100

    pull

---
+++ website-backend/resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<div class="flex flex-col">
+    <div>
+      <span class="uppercase text-sm text-gray-600 font-bold">Full Name</span>
+      <input class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+        type="text" placeholder="">
+    </div>
+    <div class="mt-8">
+      <span class="uppercase text-sm text-gray-600 font-bold">Email</span>
+      <input class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+        type="text">
+    </div>
+    <div class="mt-8">
+      <span class="uppercase text-sm text-gray-600 font-bold">Message</span>
+      <textarea
+        class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+    </div>
+    <div class="mt-8">
+      <button
+        class="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+        Send Message
+      </button>
+    </div>
+  </div>
