20c611697625ede595b08abb0ba1b6d57c79f9a2
Emmanuel Holonou <manelhln00@gmail.com>
2024-03-23 07:41:53 +0100

    New commit

--- website-backend/resources/views/newsletter.blade.php
+++ website-backend/resources/views/newsletter.blade.php
@@ -1,21 +1,26 @@
+@if (session('success'))
+    <div class="absolute bottom-4 right-4 flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <span class="sr-only">Info</span>
+        <div>
+          <span class="font-medium">{{ session('success') }}</span>
+        </div>
+    </div>
+  @endif
 <div class="relative isolate overflow-hidden bg-blue-900 py-8 sm:py-16 lg:py-24 z-0 rounded-lg">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
         <div class="max-w-xl lg:max-w-lg">
           <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Souscrivez à notre newsletter</h2>
           <p class="mt-4 text-lg leading-8 text-gray-300">Soyez les premiers informés lorsque nous avons de nouvelles offres disponible.</p>
-          @if (session('success'))
-          <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
-              {{ session('success') }}
-          </div>
-        @else
         <form action=" {{route('storeNewsletter')}} " method="post" class="mt-6 flex max-w-md gap-x-4">
           @csrf  
           <label for="email-address" class="sr-only">Addresse mail</label>
           <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="mon-contact@gmail.com">
           <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Souscrire</button>
         </form>
-        @endif
         </div>
         <dl class="grid grid-cols-1 gap-x-8 gap-y-10 lg:pt-2">
           <div class="flex flex-col items-start">
